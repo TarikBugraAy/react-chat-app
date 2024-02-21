13311dbd9faf7955810c24c8def079ca61794add
Tarık Buğra Ay <131399874+TarikBugraAy@users.noreply.github.com>
2024-02-21 12:55:01 +0300

    developping the comoponents

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -20,8 +20,8 @@ const ChatFeed = (props) => {
                     <div className="message-block">
                         {
                             isMyMessage
-                            ? <MyMessage />
-                            : <TheirMessage />
+                            ? <MyMessage message={message}/>
+                            : <TheirMessage message={message} lastMessage={messages[lastMessageKey]} />
                         }
                     </div>
                     <div className="read-receipts" style={{ marginRight: isMyMessage ? "18px" : "0px",marginLeft: isMyMessage ? "0px" : "68px" }}>
@@ -32,11 +32,23 @@ const ChatFeed = (props) => {
         })
     }
 
-    renderMessages()
+
+    if(!chat) return "Loading .....";
 
     return (
-        <div>
-            ChatFeed
+        <div className="chat-feed">
+            <div className="chat-title-container">
+                <div className="chat-title">{chat?.title}</div>
+                <div className="chat-subtitle">
+                    {chat.people.map((person) => ` ${person.person.username}` )}
+                </div>
+            </div>
+            {renderMessages()}
+            <div style={{height: "100px"}} />
+            <div className="message-form-container">
+                <MessageForm  {...props} chatId={activeChat}/>
+
+            </div>
         </div>
     )
 }
