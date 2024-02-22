70d0aec2943821c762f069b4de9e65053cc7572a
Tarık Buğra Ay <131399874+TarikBugraAy@users.noreply.github.com>
2024-02-22 14:07:16 +0300

    I think i fixed it

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,28 +1,41 @@
 const TheirMessage = ({ lastMessage, message }) => {
-  const isFirstMessageByUser = !lastMessage || (message.sender && lastMessage.sender.username !== message.sender.username);
+  let avatarElement = null;
+  let messageElement = null;
+
+  if (message.sender && message.sender.username) {
+    const isFirstMessageByUser = !lastMessage || (lastMessage.sender && lastMessage.sender.username !== message.sender.username);
+
+    if (isFirstMessageByUser) {
+      avatarElement = (
+        <div
+          className="message-avatar"
+          style={{ backgroundImage: `url(${message.sender.avatar})` }}
+        />
+      );
+    }
+
+    if (message.attachments && message.attachments.length > 0) {
+      messageElement = (
+        <img
+          src={message.attachments[0].file}
+          alt="message-attachment"
+          className="message-image"
+          style={{ marginLeft: isFirstMessageByUser ? '4px' : '48px' }}
+        />
+      );
+    } else {
+      messageElement = (
+        <div className="message" style={{ float: 'left', backgroundColor: '#CABCDC', marginLeft: isFirstMessageByUser ? '4px' : '48px' }}>
+          {message.text}
+        </div>
+      );
+    }
+  }
 
   return (
     <div className="message-row">
-      {isFirstMessageByUser && message.sender && message.sender.username && ( // Add null checks for message.sender and message.sender.username
-        <div
-          className="message-avatar"
-          style={{ backgroundImage: message.sender && `url(${message.sender.avatar})` }}
-        />
-      )}
-      {message.attachments && message.attachments.length > 0
-        ? (
-          <img
-            src={message.attachments[0].file}
-            alt="message-attachment"
-            className="message-image"
-            style={{ marginLeft: isFirstMessageByUser ? '4px' : '48px' }}
-          />
-        )
-        : (
-          <div className="message" style={{ float: 'left', backgroundColor: '#CABCDC', marginLeft: isFirstMessageByUser ? '4px' : '48px' }}>
-            {message.text}
-          </div>
-        )}
+      {avatarElement}
+      {messageElement}
     </div>
   );
 };
