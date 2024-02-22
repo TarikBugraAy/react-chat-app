eefe294d30bccccfb4165fd2527080c9a71f7bcc
Tarık Buğra Ay <131399874+TarikBugraAy@users.noreply.github.com>
2024-02-22 12:54:09 +0300

    Fixed the build problem

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,30 +1,36 @@
 const TheirMessage = ({ lastMessage, message }) => {
-    const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username;
+  const isFirstMessageByUser = !lastMessage || (message.sender && lastMessage.sender.username !== message.sender.username);
+  const senderAvatar = message.sender && message.sender.avatar;
 
-    return (
-      <div className="message-row">
-        {isFirstMessageByUser && (
-          <div
-            className="message-avatar"
-            style={{ backgroundImage: message.sender && `url(${message.sender.avatar})` }}
-          />
-        )}
-        {message.attachments && message.attachments.length > 0
-          ? (
-            <img
-              src={message.attachments[0].file}
-              alt="message-attachment"
-              className="message-image"
-              style={{ marginLeft: isFirstMessageByUser ? '4px' : '48px' }}
-            />
-          )
-          : (
-            <div className="message" style={{ float: 'left', backgroundColor: '#CABCDC', marginLeft: isFirstMessageByUser ? '4px' : '48px' }}>
-              {message.text}
-            </div>
-          )}
-      </div>
-    );
-  };
+  return (
+    <div className="message-row">
+      {isFirstMessageByUser && (
+        <div
+          className="message-avatar"
+          style={{ backgroundImage: senderAvatar ? `url(${senderAvatar})` : 'none' }}
+        />
+      )}
+      {message.attachments && message.attachments.length > 0 ? (
+        <img
+          src={message.attachments[0].file}
+          alt="message-attachment"
+          className="message-image"
+          style={{ marginLeft: isFirstMessageByUser ? '4px' : '48px' }}
+        />
+      ) : (
+        <div
+          className="message"
+          style={{
+            float: 'left',
+            backgroundColor: '#CABCDC',
+            marginLeft: isFirstMessageByUser ? '4px' : '48px'
+          }}
+        >
+          {message.text}
+        </div>
+      )}
+    </div>
+  );
+};
 
-  export default TheirMessage;
+export default TheirMessage;
